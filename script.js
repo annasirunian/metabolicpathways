18054ecaa3449f7690143951ee4f5c8cf9068487
Anna <sancho1234576@gmail.com>
2018-10-02 19:14:49 +0300

    calculate statistics

--- script.js
+++ script.js
@@ -16,6 +16,40 @@ function onShowDemo() {
   });
 }
 
+function getStatistics(data) {
+  // Nodes statistics
+  let nodeTypes = {};
+  let nodes = Object.values(data[1].nodes);
+  for (let node of nodes) {
+    let nodeType = node.node_type;
+    if (!(nodeType in nodeTypes)) {
+      nodeTypes[nodeType] = 0;
+    }
+    nodeTypes[nodeType]++;
+  }
+
+  // Genes statistics
+  let genes = {};
+  let reactions = Object.values(data[1].reactions);
+  for (let reaction of reactions) {
+    let genesInReaction = reaction.genes;
+    for (let geneInReaction of genesInReaction) {
+      let gene = geneInReaction.name;
+      if (!(gene in genes)) {
+        genes[gene] = 0;
+      }
+      genes[gene]++;
+    }
+  }
+
+  let geneNames = Object.keys(genes);
+  for (let name of geneNames) {
+    if (genes[name] <= 1) {
+      delete genes[name];
+    }
+  }
+}
+
 function handleFileSelect(evt) {
   let file = evt.target.files[0];
   let reader = new FileReader();
@@ -23,6 +57,7 @@ function handleFileSelect(evt) {
   reader.onload = function(e) {
     let data = JSON.parse(e.target.result);
     embedPathwayMap(data);
+    getStatistics(data);
   };
 
   reader.readAsText(file);
